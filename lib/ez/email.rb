0579ee2ab34fe0ecaa4350d86cc4ae1f80ec1dc1
Daniel Berger <djberg96@gmail.com>
2013-01-09 10:03:39 -0700

    Fixed a bug where the default 'from' value wasn't actually set if it wasn't specified, and refactored the tests.

--- lib/ez/email.rb
+++ lib/ez/email.rb
@@ -5,138 +5,136 @@ require 'net/smtp'
 
 # The EZ module serves as a namespace only.
 module EZ
-   
-   # The Email class encapsulates certain SMTP attributes, which are then
-   # used to send simple emails.
-   class Email
-      # The version of the ez-email library
-      VERSION = '0.1.2'
-      
-      begin
-         @@mail_host = Resolv.getaddress('mailhost')
-      rescue Resolv::ResolvError
-         @@mail_host = 'localhost'
-      end
 
+  # The Email class encapsulates certain SMTP attributes, which are then
+  # used to send simple emails.
+  class Email
+    # The version of the ez-email library
+    VERSION = '0.1.3'
+
+    begin
+      @@mail_host = Resolv.getaddress('mailhost')
+    rescue Resolv::ResolvError
+      @@mail_host = 'localhost'
+    end
+
+    @@mail_port = 25
+
+    # The name of the mail host to use when sending email. The default
+    # is whatever the address of your system's 'mailhost' resolves to.
+    # If that cannot be determined, your localhost is used.
+    #
+    def self.mail_host
+      @@mail_host
+    end
+
+    # Sets the mail host.
+    def self.mail_host=(host)
+      @@mail_host = host
+    end
+
+    # The port to use when sending email. The default is 25.
+    def self.mail_port
+      @@mail_port
+    end
+
+    # Sets the mail port.
+    def self.mail_port=(port)
       @@mail_port = 25
-      
-      # The name of the mail host to use when sending email. The default
-      # is whatever the address of your system's 'mailhost' resolves to.
-      # If that cannot be determined, your localhost is used.
-      #
-      def self.mail_host
-         @@mail_host
-      end
-      
-      # Sets the mail host.
-      def self.mail_host=(host)
-         @@mail_host = host         
-      end
-         
-      # The port to use when sending email. The default is 25.
-      def self.mail_port
-         @@mail_port         
-      end
-      
-      # Sets the mail port.
-      def self.mail_port=(port)
-         @@mail_port = 25
-      end
-      
-      # A single email address or an array of email addresses to whom
-      # the email should be sent. Mandatory.
-      attr_accessor :to
-      
-      # A single email address from whom the email is being delivered. The
-      # default is your login + '@' + your host name, though it is recommended
-      # that you specify it explicitly.
-      attr_accessor :from
-      
-      # The subject of the email. Mandatory.
-      attr_accessor :subject
-      
-      # The body of the email. Mandatory.
-      attr_accessor :body
-      
-      # Creates a new EZ::Email object. As a general rule you won't use
-      # this method, but should use EZ::Email.deliver instead.
-      #
-      def initialize(options={})
-         raise TypeError unless options.is_a?(Hash)
-         validate_options(options)
-         @options = options
-      end
-       
-      # Sends the email based on the options passed to the constructor.
-      # As a general rule you won't use this method directly, but should
-      # use EZ::Email.deliver instead.
-      #
-      def deliver
-         host = EZ::Email.mail_host
-         port = EZ::Email.mail_port
+    end
 
-         Net::SMTP.start(host, port, host){ |smtp|
-            smtp.open_message_stream(self.from, self.to){ |stream|
-               stream.puts "From: #{self.from}"
-               stream.puts "To: " + self.to.to_a.join(', ')
-               stream.puts "Subject: #{self.subject}"
-               stream.puts
-               stream.puts self.body
-            }
-         }
-      end
+    # A single email address or an array of email addresses to whom
+    # the email should be sent. Mandatory.
+    attr_accessor :to
 
-      # Delivers a simple text email message using four options:
-      #
-      # * to
-      # * from
-      # * subject
-      # * body
-      #
-      # Examples:
-      #
-      #    # Send an email to a single user
-      #    EZ::Email.deliver(
-      #       :to      => 'some_user@hotmail.com',
-      #       :from    => 'me@hotmail.com',
-      #       :subject => 'Hi',
-      #       :body    => 'How are you?'
-      #    )
-      #
-      #    # Send an email to a multiple users
-      #    EZ::Email.deliver(
-      #       :to      => ['jon@hotmail.com', 'mary@hotmail.com'],
-      #       :from    => 'me@hotmail.com',
-      #       :subject => 'Hi',
-      #       :body    => 'How are you?'
-      #    )
-      #
-      # This is a shortcut for EZ::Email.new + EZ::Email#deliver.
-      #
-      def self.deliver(options)
-         new(options).deliver
+    # A single email address from whom the email is being delivered. The
+    # default is your login + '@' + your host name, though it is recommended
+    # that you specify it explicitly.
+    attr_accessor :from
+
+    # The subject of the email. Mandatory.
+    attr_accessor :subject
+
+    # The body of the email. Mandatory.
+    attr_accessor :body
+
+    # Creates a new EZ::Email object. As a general rule you won't use
+    # this method, but should use EZ::Email.deliver instead.
+    #
+    def initialize(options={})
+      raise TypeError unless options.is_a?(Hash)
+      options[:from] ||= Etc.getlogin + '@' + Socket.gethostname
+      validate_options(options)
+      @options = options
+    end
+
+    # Sends the email based on the options passed to the constructor.
+    # As a general rule you won't use this method directly, but should
+    # use EZ::Email.deliver instead.
+    #
+    def deliver
+      host = EZ::Email.mail_host
+      port = EZ::Email.mail_port
+
+      Net::SMTP.start(host, port, host){ |smtp|
+        smtp.open_message_stream(self.from, self.to){ |stream|
+          stream.puts "From: #{self.from}"
+          stream.puts "To: " + self.to.to_a.join(', ')
+          stream.puts "Subject: #{self.subject}"
+          stream.puts
+          stream.puts self.body
+        }
+      }
+    end
+
+    # Delivers a simple text email message using four options:
+    #
+    # * to
+    # * from
+    # * subject
+    # * body
+    #
+    # Examples:
+    #
+    #    # Send an email to a single user
+    #    EZ::Email.deliver(
+    #       :to      => 'some_user@hotmail.com',
+    #       :from    => 'me@hotmail.com',
+    #       :subject => 'Hi',
+    #       :body    => 'How are you?'
+    #    )
+    #
+    #    # Send an email to a multiple users
+    #    EZ::Email.deliver(
+    #       :to      => ['jon@hotmail.com', 'mary@hotmail.com'],
+    #       :from    => 'me@hotmail.com',
+    #       :subject => 'Hi',
+    #       :body    => 'How are you?'
+    #    )
+    #
+    # This is a shortcut for EZ::Email.new + EZ::Email#deliver.
+    #
+    def self.deliver(options)
+      new(options).deliver
+    end
+
+    private
+
+    # Private method that both validates the hash options, and sets
+    # the attribute values.
+    #
+    def validate_options(hash)
+      valid = %w[to from subject body]
+
+      hash.each{ |key, value|
+        key = key.to_s.downcase
+        raise ArgumentError unless valid.include?(key)
+        send("#{key}=", value)
+      }
+
+      if to.nil? || subject.nil? || body.nil?
+        raise ArgumentError, "Missing :to, :subject or :body"
       end
-      
-      private
-      
-      # Private method that both validates the hash options, and sets
-      # the attribute values.
-      #
-      def validate_options(hash)
-         valid = %w/to from subject body/
-         hash.each{ |key, value|
-            key = key.to_s.downcase
-            raise ArgumentError unless valid.include?(key)
-            send("#{key}=", value)  
-         }
-         
-         if to.nil? || subject.nil? || body.nil?
-            raise ArgumentError, "Missing :to, :subject or :body"            
-         end
-         
-         if from.nil?
-            from = Etc.getlogin + '@' + Socket.gethostname
-         end
-      end
-   end   
+    end
+  end
 end
--- test/test_ez_email.rb
+++ test/test_ez_email.rb
@@ -3,74 +3,144 @@
 #
 # Test suite for the EZ::Email library.
 ########################################################################
-require 'test/unit'
+require 'test-unit'
 require 'ez/email'
+require 'socket'
+require 'etc'
 
 class TC_EZ_Email < Test::Unit::TestCase
-   def setup
-      @to   = 'foo@some_mail_service.com'
-      @from = 'bar@some_mail_service.com'
-      @subj = 'This is a test'
-      @body = 'How are you?'
+  def self.startup
+    @@host  = Socket.gethostname
+    @@login = Etc.getlogin
+  end
 
-      @opts = {:to => @to, :from => @from, :subject => @subj, :body => @body }
-      @email = EZ::Email.new(@opts)
-   end
-   
-   def test_version
-      assert_equal('0.1.2', EZ::Email::VERSION)      
-   end
+  def setup
+    @to   = 'foo@some_mail_service.com'
+    @from = 'bar@some_mail_service.com'
+    @subj = 'This is a test'
+    @body = 'How are you?'
 
-   def test_to
-      assert_respond_to(@email, :to)
-      assert_respond_to(@email, :to=)
-      assert_equal(@to, @email.to)
-   end
+    @opts = {:to => @to, :from => @from, :subject => @subj, :body => @body }
+    @email = EZ::Email.new(@opts)
+  end
 
-   def test_from
-      assert_respond_to(@email, :from)
-      assert_respond_to(@email, :from=)
-      assert_equal(@from, @email.from)
-   end
+  test "version is set to expected value" do
+    assert_equal('0.1.3', EZ::Email::VERSION)
+  end
 
-   def test_subject
-      assert_respond_to(@email, :subject)
-      assert_respond_to(@email, :subject=)
-      assert_equal(@subj, @email.subject)
-   end
+  test "to getter method basic functionality" do
+    assert_respond_to(@email, :to)
+    assert_nothing_raised{ @email.to }
+    assert_not_nil(@email.to)
+  end
 
-   def test_body
-      assert_respond_to(@email, :body)
-      assert_respond_to(@email, :body=)
-      assert_equal(@body, @email.body)
-   end
+  test "to getter method returns expected value" do
+    assert_equal(@to, @email.to)
+  end
 
-   def test_mail_host
-      assert_respond_to(EZ::Email, :mail_host)
-      assert_respond_to(EZ::Email, :mail_host=)
-      assert_kind_of(String, EZ::Email.mail_host)
-   end
+  test "to setter method basic functionality" do
+    assert_respond_to(@email, :to=)
+    assert_nothing_raised{ @email.to = 'bogus@some_bogus.com' }
+  end
 
-   def test_mail_port
-      assert_respond_to(EZ::Email, :mail_port)
-      assert_respond_to(EZ::Email, :mail_port=)
-      assert_equal(25, EZ::Email.mail_port)
-   end
+  test "to setter actually sets value" do
+    @email.to = 'bogus@some_bogus.com'
+    assert_equal(@email.to, 'bogus@some_bogus.com')
+  end
 
-   def test_deliver
-      assert_respond_to(EZ::Email, :deliver)
-   end
+  test "from getter basic functionality" do
+    assert_respond_to(@email, :from)
+    assert_nothing_raised{ @email.from }
+    assert_not_nil(@email.from)
+  end
 
-   def test_invalid_option_raises_error
-      assert_raise(ArgumentError){ EZ::Email.send(:new, {:bogus => 77}) }
-   end
+  test "from setter basic functionality" do
+    assert_respond_to(@email, :from=)
+    assert_nothing_raised{ @email.from = 'bogus@some_bogus.com' }
+  end
 
-   def teardown
-      @to    = nil
-      @from  = nil
-      @subj  = nil
-      @body  = nil
-      @opts  = nil
-      @email = nil
-   end
+  test "from method returns expected value" do
+    assert_equal(@from, @email.from)
+  end
+
+  test "from defaults to username@host if not set in constructor" do
+    @email = EZ::Email.new(:to => 'x', :subject => 'x', :body => 'x')
+    expected = @@login << '@' << @@host
+    assert_equal(expected, @email.from)
+  end
+
+  test "subject getter basic functionality" do
+    assert_respond_to(@email, :subject)
+    assert_nothing_raised{ @email.subject }
+    assert_not_nil(@email.subject)
+  end
+
+  test "subject setter basic functionality" do
+    assert_respond_to(@email, :subject=)
+    assert_nothing_raised{ @email.subject = 'bogus@bogus.com' }
+  end
+
+  test "subject method returns expected value" do
+    assert_equal(@subj, @email.subject)
+  end
+
+  test "body getter basic functionality" do
+    assert_respond_to(@email, :body)
+    assert_nothing_raised{ @email.body }
+    assert_not_nil(@email.body)
+  end
+
+  test "body setter basic functionality" do
+    assert_respond_to(@email, :body=)
+    assert_nothing_raised{ @email.body = "Test" }
+  end
+
+  test "body method returns the expected value" do
+    assert_equal(@body, @email.body)
+  end
+
+  test "mail_host getter basic functionality" do
+    assert_respond_to(EZ::Email, :mail_host)
+    assert_nothing_raised{ EZ::Email.mail_host }
+    assert_not_nil(EZ::Email.mail_host)
+  end
+
+  test "mail_host setter basic functionality" do
+    assert_respond_to(EZ::Email, :mail_host=)
+    assert_nothing_raised{ EZ::Email.mail_host = "Test" }
+  end
+
+  test "mail_port singleton getter basic functionality" do
+    assert_respond_to(EZ::Email, :mail_port)
+  end
+
+  test "mail_port singleton setter basic functionality" do
+    assert_respond_to(EZ::Email, :mail_port=)
+  end
+
+  test "mail_port method returns the expected value" do
+    assert_equal(25, EZ::Email.mail_port)
+  end
+
+  test "deliver singleton method basic functionality" do
+    assert_respond_to(EZ::Email, :deliver)
+  end
+
+  test "passing an invalid option to the constructor raises an error" do
+    assert_raise(ArgumentError){ EZ::Email.send(:new, {:bogus => 77}) }
+  end
+
+  def teardown
+    @to    = nil
+    @from  = nil
+    @subj  = nil
+    @body  = nil
+    @opts  = nil
+    @email = nil
+  end
+
+  def self.shutdown
+    @@host  = nil
+    @@login = nil
+  end
 end
